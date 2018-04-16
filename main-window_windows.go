2e74d6026dcaf59a27dd90b15290e33382098d77
Clarence <clarence-pan@foxmail.com>
2018-04-16 17:49:23 +0800

    优化布局

--- main-window_windows.go
+++ main-window_windows.go
@@ -30,114 +30,132 @@ func showMainWindow(args []string) {
 	isCanceled := false
 	isProcessing := false
 
+	onClickStartOptimizeBtn := func() {
+		if isProcessing {
+			walk.MsgBox(mainWin, "提示", "正在进行优化，请稍等片刻。", walk.MsgBoxIconWarning)
+			return
+		}
+
+		inFiles = uniqueLines(inFiles)
+		inTE.SetText(strings.Join(inFiles, "\r\n"))
+
+		go func() {
+			defer func() {
+				r := recover()
+
+				startOptimizeBtn.SetText("开始优化")
+				startOptimizeBtn.SetEnabled(true)
+				cancelOptimizeBtn.SetVisible(false)
+				isProcessing = false
+
+				if r != nil {
+					if isCanceled {
+						log.Print("已经取消。")
+					} else {
+						log.Print("优化时遇到错误：" + formatError(r))
+					}
+				}
+
+			}()
+
+			var ctx context.Context
+			ctx, doCancel = context.WithCancel(context.Background())
+
+			isProcessing = true
+			isCanceled = false
+			cancelOptimizeBtn.SetVisible(true)
+			cancelOptimizeBtn.SetEnabled(true)
+			startOptimizeBtn.SetEnabled(false)
+			startOptimizeBtn.SetText("正在优化...")
+			log.Print("开始优化...")
+
+			for i := 0; i < len(inFiles); i++ {
+				x := inFiles[i]
+				doOptimize(x, ctx)
+			}
+
+			log.Print("优化完成。")
+		}()
+	}
+
+	onClickCancelOptimizeBtn := func() {
+		if doCancel != nil {
+			isCanceled = true
+			doCancel()
+			doCancel = nil
+			cancelOptimizeBtn.SetEnabled(false)
+		}
+	}
+
 	mainWinDef := decl.MainWindow{
 		Title:    "图片优化工具",
 		AssignTo: &mainWin,
 		MinSize:  decl.Size{600, 400},
-		Layout:   decl.Grid{Columns: 10},
+		Layout:   decl.VBox{},
 		OnDropFiles: func(files []string) {
 			inFiles = append(inFiles, files...)
 			inTE.SetText(strings.Join(inFiles, "\r\n"))
 			log.Printf("已经收到文件，请点击【开始优化】来开始优化。")
 		},
 		Children: []decl.Widget{
-			decl.Label{
-				ColumnSpan: 10,
-				Text:       "要处理的图片文件/文件夹/压缩包列表：(请从 Windows 资源管理器往这里拖放文件...)",
-			},
-			decl.TextEdit{
-				ColumnSpan:    10,
-				AssignTo:      &inTE,
-				ReadOnly:      false,
-				Text:          "",
-				StretchFactor: 1,
-				OnTextChanged: func() {
-					text := inTE.Text()
-					inFiles = filterEmptyLines(splitIntoLines(text))
-				},
-			},
-			decl.PushButton{
-				ColumnSpan: 3,
-				AssignTo:   &startOptimizeBtn,
-				Text:       "开始优化",
-				MaxSize:    decl.Size{Width: 600, Height: 40},
-				OnClicked: func() {
-					if isProcessing {
-						walk.MsgBox(mainWin, "提示", "正在进行优化，请稍等片刻。", walk.MsgBoxIconWarning)
-						return
-					}
-
-					inFiles = uniqueLines(inFiles)
-					inTE.SetText(strings.Join(inFiles, "\r\n"))
-
-					go func() {
-						defer func() {
-							r := recover()
-
-							startOptimizeBtn.SetText("开始优化")
-							startOptimizeBtn.SetEnabled(true)
-							cancelOptimizeBtn.SetVisible(false)
-							isProcessing = false
-
-							if r != nil {
-								if isCanceled {
-									log.Print("已经取消。")
-								} else {
-									log.Print("优化时遇到错误：" + formatError(r))
-								}
-							}
-
-						}()
-
-						var ctx context.Context
-						ctx, doCancel = context.WithCancel(context.Background())
-
-						isProcessing = true
-						isCanceled = false
-						cancelOptimizeBtn.SetVisible(true)
-						cancelOptimizeBtn.SetEnabled(true)
-						startOptimizeBtn.SetEnabled(false)
-						startOptimizeBtn.SetText("正在优化...")
-						log.Print("开始优化...")
-
-						for i := 0; i < len(inFiles); i++ {
-							x := inFiles[i]
-							doOptimize(x, ctx)
-						}
-
-						log.Print("优化完成。")
-					}()
-				},
-			},
-			decl.VSpacer{
-				ColumnSpan: 4,
-			},
-			decl.PushButton{
-				ColumnSpan: 3,
-				AssignTo:   &cancelOptimizeBtn,
-				Text:       "取消",
-				MaxSize:    decl.Size{Width: 600, Height: 40},
-				Visible:    false,
-				OnClicked: func() {
-					if doCancel != nil {
-						isCanceled = true
-						doCancel()
-						doCancel = nil
-						cancelOptimizeBtn.SetEnabled(false)
-					}
-				},
-			},
-			// decl.VSpacer{},
-
-			decl.Label{
-				ColumnSpan: 10,
-				Text:       "优化日志：",
-			},
 			decl.VSplitter{
-				ColumnSpan:    10,
-				StretchFactor: 1,
-				// Background:    decl.SolidColorBrush{Color: walk.RGB(255, 0, 0)},
-				AssignTo:      &logViewParent,
+				HandleWidth: 3,
+				// Background:  decl.SolidColorBrush{Color: walk.RGB(255, 0, 0)},
+				Children: []decl.Widget{
+					decl.Composite{
+						Layout: decl.Grid{Columns: 10},
+						Children: []decl.Widget{
+							decl.Label{
+								ColumnSpan: 10,
+								Text:       "要处理的图片文件/文件夹/压缩包列表：(请从 Windows 资源管理器往这里拖放文件...)",
+							},
+							decl.TextEdit{
+								ColumnSpan:    10,
+								AssignTo:      &inTE,
+								ReadOnly:      false,
+								Text:          "",
+								StretchFactor: 1,
+								OnTextChanged: func() {
+									text := inTE.Text()
+									inFiles = filterEmptyLines(splitIntoLines(text))
+								},
+							},
+							decl.PushButton{
+								ColumnSpan: 3,
+								AssignTo:   &startOptimizeBtn,
+								Text:       "开始优化",
+								MaxSize:    decl.Size{Width: 600, Height: 40},
+								OnClicked:  onClickStartOptimizeBtn,
+							},
+							decl.VSpacer{
+								ColumnSpan: 4,
+							},
+							decl.PushButton{
+								ColumnSpan: 3,
+								AssignTo:   &cancelOptimizeBtn,
+								Text:       "取消",
+								MaxSize:    decl.Size{Width: 600, Height: 40},
+								Visible:    false,
+								OnClicked:  onClickCancelOptimizeBtn,
+							},
+						},
+					},
+					decl.Composite{
+						Layout: decl.Grid{Columns: 10},
+						Children: []decl.Widget{
+							decl.Label{
+								ColumnSpan: 10,
+								Text:       "优化日志：",
+							},
+							decl.VSplitter{
+								ColumnSpan:    10,
+								StretchFactor: 1,
+								// Background:    decl.SolidColorBrush{Color: walk.RGB(255, 0, 0)},
+								AssignTo: &logViewParent,
+							},
+						},
+					},
+				},
 			},
 		},
 	}
